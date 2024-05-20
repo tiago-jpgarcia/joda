a607b9cdcd444c44fe37e1053154d950b6af76c9
Tiago Garcia <tiagojpgarcia@gmail.com>
2024-05-20 22:07:30 +0100

    add media queries to wrapimages

--- src/components/Organisms/WrapContainer.vue
+++ src/components/Organisms/WrapContainer.vue
@@ -6,14 +6,16 @@
     <div class="middle-container" :style="middleStyle">
       <slot name="middle"></slot>
     </div>
-    <div class="right-container" :style="rightStype">
+    <div class="right-container" :style="rightStyle">
       <slot name="right"></slot>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive, computed } from 'vue'
+import { ref, reactive, computed, onMounted, onUnmounted } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+
 export default {
   name: 'TextComponent',
   props: {
@@ -35,7 +37,29 @@ export default {
     }
   },
   setup(props) {
+    const { width } = useWindowSize()
+    const reactiveWidth = ref(width)
+
+    const isMobile = computed(() => {
+      console.log('aqui', reactiveWidth.value <= 1200)
+      return reactiveWidth.value <= 1200
+    })
+
+    // Update reactiveWidth whenever the window is resized
+    const updateWidth = () => {
+      reactiveWidth.value = window.innerWidth
+    }
+
+    onMounted(() => {
+      window.addEventListener('resize', updateWidth)
+    })
+
+    onUnmounted(() => {
+      window.removeEventListener('resize', updateWidth)
+    })
+
     props = reactive(props)
+
     return {
       style: computed(() => ({
         flexDirection: props.column ? 'column' : 'row',
@@ -44,15 +68,15 @@ export default {
       })),
       leftStyle: computed(() => ({
         flex: props.fullWidth ? '1 0 auto' : '',
-        width: props.column && props.fullWidth ? '100%' : '33%'
+        width: props.column && props.fullWidth ? '100%' : isMobile.value ? '100%' : '33%'
       })),
       middleStyle: computed(() => ({
         flex: props.fullWidth ? '1 0 auto' : '',
-        width: props.column && props.fullWidth ? '100%' : '33%'
+        width: props.column && props.fullWidth ? '100%' : isMobile.value ? '100%' : '33%'
       })),
-      rightStype: computed(() => ({
+      rightStyle: computed(() => ({
         flex: props.fullWidth ? '1 0 auto' : '',
-        width: props.column && props.fullWidth ? '100%' : '33%'
+        width: props.column && props.fullWidth ? '100%' : isMobile.value ? '100%' : '33%'
       }))
     }
   }
@@ -72,5 +96,16 @@ export default {
   padding-bottom: 1rem;
   padding-left: 0.5rem;
   padding-right: 0.5rem;
+}
+
+@media (max-width: 1200px) {
+  .wrap-container {
+    display: block;
+  }
+  .wrap-container .right-container,
+  .wrap-container .left-container,
+  .wrap-container .middle-container {
+    width: 100%;
+  }
 }
 </style>
